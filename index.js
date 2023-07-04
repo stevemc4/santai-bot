6fc052c7d2c7d9baa07ec13d663d63a2ec0f5963
Dhika Rizky <deka_r@outlook.com>
2023-07-05 00:55:36 +0700

    Add request handler

--- index.js
+++ index.js
@@ -1,21 +1,33 @@
 import dotenv from 'dotenv'
+import { createApp, eventHandler, toNodeListener } from 'h3';
+import { listen } from 'listhen';
 
 dotenv.config()
+const app = createApp();
 
-await fetch(
-  process.env.SANTAI_WEBHOOK_URL,
-  {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      content: 'Senin? Gue sih santai',
-      embeds: [{
-        image: {
-          url: 'https://assets.change.org/photos/4/qx/wv/qyqxwVxhrtfoeny-800x450-noPad.jpg?1538395188'
-        }
-      }]
-    })
-  }
-)
+app.use(
+  '/',
+  eventHandler(async () => {
+    await fetch(
+      process.env.SANTAI_WEBHOOK_URL,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          content: 'Senin? Gue sih santai',
+          embeds: [{
+            image: {
+              url: 'https://assets.change.org/photos/4/qx/wv/qyqxwVxhrtfoeny-800x450-noPad.jpg?1538395188'
+            }
+          }]
+        })
+      }
+    )
+
+    return { 'status': 'ok' }
+  })
+);
+
+listen(toNodeListener(app));
